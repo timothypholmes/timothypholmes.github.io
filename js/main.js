e872d3ce735935444e869e9eab9b941cce5f6e7f
Timothyholmes11 <tpholmes7@gmail.com>
2019-05-07 20:07:42 -0500

    Banner Update

--- js/main.js
+++ js/main.js
@@ -12,3 +12,25 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 } 
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 500) {
+  $('.nav').css('background','#222226');
+  } else {
+  $('.nav').css('background','transparent');
+  }
+  });
+
+/*
+$(document).on('scroll', function (e) {
+  $('.banner').css('opacity', ($(document).scrollTop() / 500));});
+*/
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
