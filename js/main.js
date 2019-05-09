73813c2e57b79d9a5620dfefd347480919d935a6
Timothyholmes11 <tpholmes7@gmail.com>
2019-05-08 21:10:02 -0500

    Fixed Collapsible Bug

--- js/main.js
+++ js/main.js
@@ -19,21 +19,6 @@ for (i = 0; i < coll.length; i++) {
   });
 } 
 
-var coll = document.getElementsByClassName("collapsible-projects");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-} 
-
 /* Scrolling turns nav bar transparent --> gray */
 $(window).scroll(function () {
   if ($(window).scrollTop() >= 500) {
