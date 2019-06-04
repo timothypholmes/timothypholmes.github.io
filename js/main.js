8832747efc7ffc75481b415b9025ae7f3defd6f1
Timothyholmes11 <tpholmes7@gmail.com>
2019-06-04 13:07:24 -0500

    Timeline Update

--- js/main.js
+++ js/main.js
@@ -42,3 +42,22 @@ function myFunction() {
     x.style.display = "block";
   }
 } 
+
+(() => {
+  'use strict';
+  // Page is loaded
+  const objects = document.getElementsByClassName('banner');
+
+  Array.from(objects).map((item) => {
+    // Start loading image
+    const img = new Image();
+    img.src = item.dataset.src;
+    // Once image is loaded replace the src of the HTML element
+    img.onload = () => {
+      item.classList.remove('banner');
+      return item.nodeName === 'IMG' ? 
+        item.src = item.dataset.src :        
+        item.style.backgroundImage = `url(${item.dataset.src})`;
+    };
+  });
+})();
