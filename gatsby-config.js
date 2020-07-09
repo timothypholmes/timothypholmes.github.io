091d79b02e5e5637a5eb6cf509b29d52769bd5c7
Tim Holmes <tpholmes7@gmail.com>
2020-07-08 22:00:10 -0400

    Update Test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  pathPrefix: "./src/pages/",
-  /*
+  //pathPrefix: "/src/pages",
+  
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -22,7 +22,7 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/`,
+        start_url: `/src/pages`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
@@ -33,5 +33,5 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-  */
+  
 }
